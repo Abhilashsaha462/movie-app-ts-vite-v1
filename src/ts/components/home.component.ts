cb5be19f3639e27b706a618ddf9fe910eb9d59db
Abhilashsaha462 <108723567+Abhilashsaha462@users.noreply.github.com>
2026-03-14 22:56:40 +0530

    AS-14-03-2026-Message: 1. Added footer.component.ts file. 2. Added home.component.ts file. 3. Updated main.ts file.

--- src/ts/components/home.component.ts
+++ src/ts/components/home.component.ts
@@ -1,4 +1,6 @@
 import HomeStore from "../store/home.store";
+import type { TAlerts } from "../types/alert.type";
+import type { TEventNames } from "../types/events.type";
 
 const HomeComponent = (): string => {
     return `
@@ -20,7 +22,7 @@ const HomeComponent = (): string => {
           </div>
           <span class="ms-2">Searching...</span>
         </div>
-        
+
         <div id="home_info_text" class="mt-4">
             <div class="row g-4">
                 <div class="col-12">
@@ -35,10 +37,79 @@ const HomeComponent = (): string => {
 }
 
 export const HomeComponentEvents = () => {
-    const input = document.querySelector<HTMLInputElement>("#search-input");
-    input?.addEventListener("input", () => {
-        HomeStore.searchInputValue = input.value;
+    StoreNativeHTMLElements();
+    HomeStore.searchInputEvents.forEach((event) => {
+        HomeStore.searchInput?.addEventListener(event, (e: InputEvent | KeyboardEvent) => {
+            if (e.type as TEventNames === "input") HandleSearchInputTyping(e as InputEvent);
+            else if (e.type as TEventNames === "keypress") HandleKeyPressEvent(e as KeyboardEvent);
+        });
     });
+    HomeStore.searchBtn?.addEventListener("click", HandleSearch);
+    HomeStore.clearBtn?.addEventListener("click", HandleClear);
 };
+
+const StoreNativeHTMLElements = () => {
+    HomeStore.searchInput = document.querySelector<HTMLInputElement>("#search-input");
+    HomeStore.searchBtn = document.querySelector<HTMLButtonElement>("#search-btn");
+    HomeStore.clearBtn = document.querySelector<HTMLButtonElement>("#clear-btn");
+    HomeStore.searchAlert = document.querySelector<HTMLDivElement>("#search-alert");
+    HomeStore.searchLoader = document.querySelector<HTMLDivElement>("#search-loader");
+}
+
+const ShowLoader = (status: boolean) => {
+    const loader = HomeStore.searchLoader;
+    if (!loader) return;
+    if (status) {
+        loader.classList.remove("d-none");
+        loader.classList.add("d-flex", "justify-content-center", "align-items-center", "fs-4");
+    } else {
+        loader.classList.remove("d-flex", "justify-content-center", "align-items-center", "fs-4");
+        loader.classList.add("d-none");
+    }
+};
+
+const ShowAlert = (message: string, type: TAlerts) => {
+    const alertElement = document.createElement("div");
+    alertElement.className = `alert alert-${type} alert-dismissible fade show`;
+    alertElement.setAttribute("role", "alert");
+    alertElement.innerHTML = `
+        <div>${message}</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    `;
+    HomeStore.searchAlert?.appendChild(alertElement);
+};
+
+const HandleSearch = async () => {
+    const infoText = document.querySelector<HTMLDivElement>("#home_info_text");
+    const query = HomeStore.searchInputValue.trim();
+    infoText?.classList.toggle("d-none");
+    ShowLoader(true);
+    if (query === undefined || query === null || query === "") {
+        ShowAlert("Please enter a valid input", "warning");
+    }
+    const timeoutTask = setTimeout(() => {
+        ShowLoader(false);
+        infoText?.classList.toggle("d-none");
+        clearTimeout(timeoutTask);
+    }, 500);
+}
+
+const HandleClear = () => {
+    HomeStore.searchInput!.value = "";
+    HomeStore.searchInput!.dispatchEvent(new Event("input", { bubbles: true }));
+}
+
+const HandleSearchInputTyping = (e: InputEvent) => {
+    const input = e.target as HTMLInputElement;
+    HomeStore.searchInputValue = input.value;
+    HomeStore.clearBtn?.classList.toggle("d-none", HomeStore.searchInputValue.length === 0);
+};
+
+const HandleKeyPressEvent = (e: KeyboardEvent) => {
+    if (e.key.toLowerCase() === "enter") {
+        e.preventDefault();
+        HandleSearch();
+    }
+}
 
 export default HomeComponent;
